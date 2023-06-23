625dcfc98250324e4a09478059aef8f391dd67bb
antonino <antonino.nolano@gmail.com>
2023-06-23 14:49:05 +0200

    More comments

--- src/server.rs
+++ src/server.rs
@@ -9,7 +9,9 @@ pub mod orderbook {
 }
 
 #[derive(Debug, Default)]
-pub struct MyOrderbookAggregator {}
+pub struct MyOrderbookAggregator {
+    // register to book for updates
+}
 
 #[tonic::async_trait]
 impl OrderbookAggregator for MyOrderbookAggregator {
@@ -26,9 +28,34 @@ impl OrderbookAggregator for MyOrderbookAggregator {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
+
+    // read commandline configuration currency pair
+
+    // create queue (mpsc)
+
+    // initialize book object
+        // expose method for register subscribers
+        // dequeue orders from queue
+        // merge into BinaryHeap
+
+    // binance driver
+        // connects to exchange
+        // receives orders, normalize and push into queue
+
+    // bitstamp driver
+        // connects to exchange
+        // receives orders, normalize and push into queue
+
+
+    // aggregator
+        // register for updates to book
+        // publish summary on grpc
     let aggregator = MyOrderbookAggregator::default();
 
+    // setup address for grpc server binding
+    let addr = "[::1]:50051".parse()?;
+
+    // mainLoop: run grpc server
     Server::builder()
         .add_service(OrderbookAggregatorServer::new(aggregator))
         .serve(addr)
